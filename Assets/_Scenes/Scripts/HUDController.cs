0fdcac802c2823b51aef229c5f86861ad6998228
João Teló <joaotelo.nh@hotmail.com>
2015-03-14 01:27:27 -0300

    HUD Score

---
+++ Assets/_Scenes/Scripts/HUDController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDController : MonoBehaviour {
+
+	float score = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponentInChildren<Text> ().text = "Score: " + score.ToString ();
+	}
+
+	public void setScore(float score){
+		this.score = score;
+	}
+
+	public float getScore(){
+		return this.score;
+	}
+
+}
