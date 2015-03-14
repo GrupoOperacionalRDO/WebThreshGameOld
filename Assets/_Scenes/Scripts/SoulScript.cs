306018054c50c9f91af328a1f4021ef5fb73a070
arturnista <arturnista@gmail.com>
2015-03-14 01:28:53 -0300

    Seila

---
+++ Assets/_Scenes/Scripts/SoulScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoulScript : MonoBehaviour {
+
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Rigidbody> ().velocity = transform.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	protected void OnTriggerEnter(Collider other){
+		Destroy (this.gameObject);
+	}
+}
