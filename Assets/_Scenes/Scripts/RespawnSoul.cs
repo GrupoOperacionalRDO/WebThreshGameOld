3e50c2f60079d636c97ab16043876f9e24a49e4e
arturnista <arturnista@gmail.com>
2015-03-14 12:02:56 -0300

    activeRespawn criado

--- Assets/_Scenes/Scripts/RespawnSoul.cs
+++ Assets/_Scenes/Scripts/RespawnSoul.cs
@@ -5,6 +5,8 @@ public class RespawnSoul : MonoBehaviour {
 	private float z;
 	private int countConstante;
 	private int countTroca;
+	// public somente para testes, quando for usar realmente passar para private e usar o setActiveRespawn
+	public bool activeRespawn;
 	public GameObject soul;
 
 	// Use this for initialization
@@ -13,12 +15,18 @@ public class RespawnSoul : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (++countConstante >= countTroca) 
-		{
-			z = Random.Range (-1.0F, 10.0F);
-			Instantiate (soul, new Vector3 (-10, 1, z), Quaternion.identity);
-			countConstante = 0;
-			countTroca = Random.Range (15, 40);
-		}
+		if (activeRespawn)
+			if (++countConstante >= countTroca) 
+			{
+				z = Random.Range (-1.0F, 10.0F);
+				Instantiate (soul, new Vector3 (-10, 1, z), Quaternion.identity);
+				countConstante = 0;
+				countTroca = Random.Range (15, 40);
+			}
+	}
+
+	public void setActiveRespawn(bool newActiveRespawn)
+	{
+		activeRespawn = newActiveRespawn;
 	}
 }
