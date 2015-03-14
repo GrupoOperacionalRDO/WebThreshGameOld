87a761e97c8bf5c73b59403dab2c9c40eadab41f
arturnista <arturnista@gmail.com>
2015-03-14 03:53:20 -0300

    Script de Geração dos Souls + Movimento dos Souls

---
+++ Assets/_Scenes/Scripts/RespawnSoul.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnSoul : MonoBehaviour {
+	private float z;
+	private int countConstante;
+	private int countTroca;
+	public GameObject soul;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (++countConstante >= countTroca) 
+		{
+			z = Random.Range (-1.0F, 10.0F);
+			Instantiate (soul, new Vector3 (-10, 1, z), Quaternion.identity);
+			countConstante = 0;
+			countTroca = Random.Range (15, 40);
+		}
+	}
+}
