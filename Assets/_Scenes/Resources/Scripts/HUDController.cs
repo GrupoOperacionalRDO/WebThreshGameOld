6cc2842ee21c5f39a968523b9cbd01e053e348b8
João Teló <joaotelo.nh@hotmail.com>
2015-03-14 22:25:31 -0300

    folder structure and another hudcontrollertests

--- Assets/_Scenes/Resources/Scripts/HUDController.cs
+++ Assets/_Scenes/Resources/Scripts/HUDController.cs
@@ -1,4 +1,13 @@
-﻿using UnityEngine;
+﻿/**	HUDController.cs
+ *  @author: João Teló
+ *   
+ *	Utilizar esse componente para exibir a quantidade de pontos,
+ *	colocando ele em um Canvas, que tenha como filho um Text.
+ *	O acesso a variável score não é permitido diretamente, controle
+ *  ela por setScore, getScore, addScore e removeScore.
+ */
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -6,12 +15,14 @@ public class HUDController : MonoBehaviour {
 
 	float score = 0;
 
-	// Use this for initialization
+	/** Start e Update são utilizados para sincronizar o valor da variável score
+	 *  com o componente Texto da HUD.
+	 */
+
 	void Start () {
 		gameObject.GetComponentInChildren<Text> ().text = "Score: " + score.ToString ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		gameObject.GetComponentInChildren<Text> ().text = "Score: " + score.ToString ();
 	}
@@ -23,6 +34,15 @@ public class HUDController : MonoBehaviour {
 
 	public float getScore(){
 		return this.score;
+	}
+
+	public void addScore(float points){
+		this.score += points;
+		this.Update();
+	}
+
+	public void removeScore(float points){
+		this.addScore(-points);
 	}
 
 }
--- Assets/_TestScenes/Scripts/Editor/HUDControllerTest.cs
+++ Assets/_TestScenes/Scripts/Editor/HUDControllerTest.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/**	HUDControllerTest.cs
+ *  @author: João Teló
+ *   
+ *	Utilizado para testar os métodos do HUDController.
+ *
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using NUnit.Framework;
@@ -23,5 +30,32 @@ namespace UnityTest
 			Assert.That(hud.GetComponentInChildren<UnityEngine.UI.Text>().text,Is.EqualTo("Score: " + randomScore.ToString()));
 			GameObject.DestroyImmediate (hud);
 		}
+
+		[Test]
+		public void TestAddScoreText(){
+			//Preparation
+			GameObject hud = GameObject.Instantiate(Resources.Load("Prefabs/HUD")) as GameObject;
+			float randomScore = UnityEngine.Random.value * 100;
+			//Action
+			hud.GetComponent<HUDController> ().setScore (randomScore);
+			hud.GetComponent<HUDController> ().addScore (randomScore);
+			//Assertion
+			Assert.That(hud.GetComponentInChildren<UnityEngine.UI.Text>().text,Is.EqualTo("Score: " + (2 * randomScore).ToString()));
+			GameObject.DestroyImmediate (hud);
+		}
+
+		[Test]
+		public void TestRemoveScoreText(){
+			//Preparation
+			GameObject hud = GameObject.Instantiate(Resources.Load("Prefabs/HUD")) as GameObject;
+			float randomScore = UnityEngine.Random.value * 100;
+			//Action
+			hud.GetComponent<HUDController> ().setScore (randomScore);
+			hud.GetComponent<HUDController> ().removeScore (randomScore);
+			//Assertion
+			Assert.That(hud.GetComponentInChildren<UnityEngine.UI.Text>().text,Is.EqualTo("Score: 0"));
+			GameObject.DestroyImmediate (hud);
+		}
+
 	}
 }
